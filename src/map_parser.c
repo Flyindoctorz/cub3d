59cdabdd9ab36fbc5ab80109e2a8cfb73d316118
Corentin Gelgon <cgelgon@f4r7s8.clusters.42paris.fr>
2025-06-04 12:26:54 +0200

    map reader parser advanced

--- src/map_parser.c
+++ src/map_parser.c
@@ -6,7 +6,7 @@
 /*   By: cgelgon <cgelgon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 13:52:35 by cgelgon           #+#    #+#             */
-/*   Updated: 2025/06/03 14:18:13 by cgelgon          ###   ########.fr       */
+/*   Updated: 2025/06/04 12:26:39 by cgelgon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,4 +41,43 @@ char	**extract_map_lines(char **lines, int start, int end)
 		i++;
 	}
 	return (map);
+}
+
+static bool	get_map_data(char **line, int start, int end, t_data *data)
+{
+	int		i;
+	int		curr_width;
+
+	if (!line || !data)
+		return (printf("Invalid input to get_map_data"), false);
+	data->map.map = extract_map_lines(line, start, end);
+	if (!data->map.map)
+		return (printf("Failed to extract map lines"), false);
+	data->map.height = line_height(data->map.map);
+	data->map.width = 0;
+	i = 0;
+	while (data->map.map[i])
+	{
+		curr_width = line_width(data->map.map[i]);
+		if (curr_width > data->map.width)
+			data->map.width = curr_width;
+		i++;
+	}
+	return (true);
+}
+
+bool	parse_map_section(char **lines, t_data *data)
+{
+	int		start;
+	int		end;
+
+	if (!lines || !data)
+		return (printf("Invalid input to parse_map_section"), false);
+	start = find_map_start(lines);
+	if (start == -1)
+		return (printf("Map start not found"), false);
+	end = find_map_end(lines, start);
+	if (end == -1)
+		return (printf("Map end not found"), false);
+	return (get_map_data(lines, start, end, data));
 }
--- src/parsing_core/utils_for_parser.c
+++ src/parsing_core/utils_for_parser.c
@@ -6,7 +6,7 @@
 /*   By: cgelgon <cgelgon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 13:58:42 by cgelgon           #+#    #+#             */
-/*   Updated: 2025/06/02 14:06:35 by cgelgon          ###   ########.fr       */
+/*   Updated: 2025/06/04 12:19:08 by cgelgon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,13 +78,13 @@ void	init_it_all(t_data *data)
 	data->texture.south.id = "SO";
 	data->texture.west.id = "WE";
 	data->texture.east.id = "EA";
-	data->floor.r = 0;
-	data->floor.g = 0;
-	data->floor.b = 0;
+	data->floor.red = 0;
+	data->floor.green = 0;
+	data->floor.blue = 0;
 	data->floor.hex = -1;
-	data->ceiling.r = 0;
-	data->ceiling.g = 0;
-	data->ceiling.b = 0;
+	data->ceiling.red = 0;
+	data->ceiling.green = 0;
+	data->ceiling.blue = 0;
 	data->ceiling.hex = -1;
 	data->map.map = NULL;
 	data->map.width = 0;
@@ -95,6 +95,27 @@ void	init_it_all(t_data *data)
 }
 
 
+void	parse_config_section(char **lines, int map_start, t_data *data)
+{
+	int	i;
 
+	if (!lines || !data || map_start < 0)
+		return (printf("Error: Invalid input to parse_config_section\n"), false);
+	i = 0;
+	while (i < map_start)
+	{
+		if (is_empty_or_comment(lines[i]))
+		{
+			i++;
+			continue;
+		}
+		if (!parse_a_color_line(lines[i], data))
+			parse_texture(lines[i], data);
+		i++;
+	}
+	if (data->texture.north.path == NULL || data->texture.south.path == NULL
+		|| data->texture.west.path == NULL || data->texture.east.path == NULL)
+		printf("Error: Missing texture paths\n");
+}
 
 
