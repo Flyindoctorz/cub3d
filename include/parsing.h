18311d0785fbde05da5e554711b332b539baf83b
Corentin Gelgon <cgelgon@f4r7s8.clusters.42paris.fr>
2025-06-05 12:02:52 +0200

    .h finished, need parse cub function

--- include/parsing.h
+++ include/parsing.h
@@ -6,7 +6,7 @@
 /*   By: cgelgon <cgelgon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 13:15:41 by cgelgon           #+#    #+#             */
-/*   Updated: 2025/06/05 10:55:10 by cgelgon          ###   ########.fr       */
+/*   Updated: 2025/06/05 12:00:47 by cgelgon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,10 @@
 # include "libft.h"
 # include "raycasting.h"
 
+/* ************************************************************************** */
+/*                                STRUCTURES                                  */
+/* ************************************************************************** */
 
-# define BUFFER_SIZE 4096
-
-// structures
 typedef struct s_map
 {
 	char			**map;
@@ -56,7 +56,6 @@ typedef struct s_color
 	int				hex;
 }				t_color;
 
-
 typedef struct s_parser_status
 {
 	bool				all_texture_valid;
@@ -64,76 +63,122 @@ typedef struct s_parser_status
 	bool				map_valid;
 }					t_parser_status;
 
+/* ************************************************************************** */
+/*                               INIT		                                  */
+/* ************************************************************************** */
 
-// included 
 int					init_data(t_data *data);
 void				init_texture (t_texture *texture);
-// file reading
+
+/* ************************************************************************** */
+/*                               FILE READER		                          */
+/* ************************************************************************** */
+
 char				*read_file_content(int fd);
 char				**read_file_lines(const char *filename);
 char				*read_file_content(int fd);
 char				*read_it_full(char *filename);
 
-// map utils
+/* ************************************************************************** */
+/*                               MAP UTILS		                              */
+/* ************************************************************************** */
+
 char				**copy_map(char **map);
 void				free_copy(char **map_copy);
 int					line_width(char *line);
 int					line_height(char **map);
 bool				is_in_map(char **map, int x, int y);
-char				is_a_valid_char(char c);
 
-// map validation
+/* ************************************************************************** */
+/*                               MAP VALID		                              */
+/* ************************************************************************** */
+
 bool				is_a_valid_char(char c);
 bool				is_valid_map(char **map);
+bool				is_valid_player(char c);
 bool				where_player_at(char **map, int *p_x, int *p_y);
 bool				validate_map(char **map);
 
-// map closure
+/* ************************************************************************** */
+/*                               MAP CLOSE		                              */
+/* ************************************************************************** */
+
 void				flood_fill(char **map, int x, int y, int *valid);
 bool				check_map_close(char **map);
 
-// texture handling
-bool				is_texture_line(char *line, t_texture_id *texture_id);
-bool				extract_texture_path(char *line, t_texture_id *texture_id);
-bool				parse_texture(char *line, t_texture *texture);
+/* ************************************************************************** */
+/*                               TEXTURE VALID	                              */
+/* ************************************************************************** */
+
+bool				validate_file(const char *path);
 bool				validate_xpm(const char *path);
 bool				validate_single_texture(const char *path, const char *texture_name);
 bool				validate_textures(t_texture *texture);
+bool				validate_texture_file(const char *path);
+bool				validate_all_textures(t_texture *texture);
 bool				double_check_texture(t_texture *texture);
 
-// texture parser
+/* ************************************************************************** */
+/*                               TEXTURE PARSER	                              */
+/* ************************************************************************** */
+
+bool				is_texture_line(char *line, t_texture_id *texture_id, int *pos);
+bool				extract_texture_path(char *line, int start_pos, t_texture_id *texture_id);
 bool				try_to_parse_texture(char *line, t_texture_id *texture_id);
 bool				parse_one_texture(char *line, t_texture *texture);
 bool				parse_all_textures(char **lines, int end, t_data *data);
-bool				validate_texture_file(const char *path);
-bool				validate_all_textures(t_texture *texture);
 
-// Utils for cub. line parsing
-bool				is_empty_line(char *line);
-bool				is_comment_line(char *line);
-bool				is_config_line(char *line);
-bool				is_map_line(char *line);
-void				free_string_array(char **array);
+/* ************************************************************************** */
+/*                               COLOR PARSER	                              */
+/* ************************************************************************** */
 
-// Map parser utils
+bool				is_color_line(char *line, char letter, int *pos);
+bool				parse_rgb_component(char *line, int *pos, int *components, int component_id);
+bool				parse_color_line(char *line, t_color *color);
+bool				is_valid_rgb_number(int value);
+void				color_to_hex(t_color *color);
+
+/* ************************************************************************** */
+/*                               MAP PARSER	                              */
+/* ************************************************************************** */
+
+char				**extract_map_lines(char **lines, int start, int end);
+bool				parse_map_section(char **lines, t_data *data);
 int					find_map_start(char **lines);
 int					find_map_end(char **lines, int start);
 int					count_map_lines(char **lines, int start, int end);
 
-// Utils for parser
-bool	is_empty_or_comment(char *line);
-float	calculate_map_char_percentage(char *line);
-int		ft_strotoi(char *str, char **endptr);
-void	init_it_all(t_data *data);
-void	parse_config_section(char **lines, int map_start, t_data *data);
+/* ************************************************************************** */
+/*                               PARSING UTILS	                              */
+/* ************************************************************************** */
+
+bool				is_empty_line(char *line);
+bool				is_comment_line(char *line);
+bool				is_config_line(char *line);
+bool				is_map_line(char *line);
+bool				is_empty_or_comment(char *line);
+void				free_string_array(char **array);
+int					ft_strotoi(char *str, char **endptr);
+void				init_it_all(t_data *data);
+void				parse_config_section(char **lines, int map_start, t_data *data);
+
+/* ************************************************************************** */
+/*                               PARSING CORE	                              */
+/* ************************************************************************** */
+
+void				clean_parsing_data(t_data *data);
+bool				parse_single_cubline(char **line, t_data *data);
+bool				validate_all_datas(t_data *data);
+bool				parse_and_config_map(const char *filename, t_data *data);
+
+
+
+
+
+float				calculate_map_char_percentage(char *line);
+
 
 // parsing core I
-void	clean_parsing_data(t_data *data);
-bool	parse_single_cubline(char **line, t_data *data);
-bool	validate_all_datas(t_data *data);
-
-// parsing core II
-bool	parse_and_config_map(const char *filename, t_data *data);
 bool	parse_cub_file(char **lines, t_data *data);
 
 
